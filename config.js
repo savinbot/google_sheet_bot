e4ff8e81de5503e8b7976c25f6c7b9a0cda1ffbe
Anton <33631140+savinbot@users.noreply.github.com>
2019-05-18 02:11:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
- 'TelegramProductionURL': 'https://YOUR-PRODUCTION-URL/', // do not forget trailing "/" !!
+ 'TelegramProductionURL': 'https://googlesheetbot.herokuapp.com/', // do not forget trailing "/" !!
  'TelegramToken': '708314296:AAHtkcT2PG-BR_I0Ayo2LmCdpNSJrf22IuI', // Telegram token
  'googleSheetKey': '2PACX-1vQHQ_nfTa3951QpGyFGva7k2a4n4m5shRQ1qojfNxJypIbx4QHhbV0QEF1K9jWmZO3GO4d3isPQLsJt', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
